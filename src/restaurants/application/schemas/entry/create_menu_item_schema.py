63effa46c4b50b3af306489b8218fc9aa6f7121f
Rymedero <rymedero.21@est.ucab.edu.ve>
2025-07-04 00:08:46 -0400

    feat: :sparkles: Introduced MenuCategory enum for menu item categories

--- src/restaurants/application/schemas/entry/create_menu_item_schema.py
+++ src/restaurants/application/schemas/entry/create_menu_item_schema.py
@@ -1,18 +1,24 @@
 
 
 
+from enum import Enum
 from uuid import UUID
 from pydantic import BaseModel, Field
 from typing import Optional
 from pydantic.types import UUID4
 
-
+class MenuCategory(str, Enum):
+    """Enumeration for menu categories."""
+    Entrada = "Entrada"
+    Principal = "Principal"
+    Postre = "Postre"
+    Bebida = "Bebida"
 class CreateMenuItemSchema(BaseModel):
     """Schema for creating a new menu item."""
 
     name: str = Field(..., description="Name of the menu item", min_length=2)
     description: str = Field(..., description="Description of the menu item", min_length=5)
-    category: str = Field(..., description="Category of the menu item", min_length=2)
+    category: MenuCategory = Field(..., description="Category of the menu item", min_length=2)
     restaurant_id: UUID | None = Field(
         default=None,
         description="ID of the restaurant to which the menu item belongs"
